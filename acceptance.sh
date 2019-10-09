0e0a979b03670169d8818605e2e662588b687ddc
Khurram Baig <kbaig@redhat.com>
2019-10-09 18:21:14 +0530

    Add Install for ab

--- acceptance.sh
+++ acceptance.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 
+go build -o main .
+
+./main &
+PROC_ID=$!
+
 curl 'http://localhost:8080/save/doc'
 curl 'http://localhost:8080/save/doc'  -H 'Content-Type: application/x-www-form-urlencoded'    --data 'body=hellotestbenchwashere'
 
 acceptedRate=1000
 
+if ! [ -x "$(command -v ab)" ]; then
+  echo installing ab
+  apk --no-cache add apache2-utils
+fi
+
+
 if [[ $(printf  %.0f `ab -k  -T 'application/x-www-form-urlencoded'    -n 1000 -c 1 'http://localhost:8080/view/doc' | grep 'Requests per second' | grep -oE [0-9]+.[0-9]+ `) -gt acceptedRate ]] 
 then
+    kill -9 $PROC_ID
     echo PASS
 else
+   kill -9 $PROC_ID
    echo Acceptance Criteria Failed
    exit 1
 fi
